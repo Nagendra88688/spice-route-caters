47ae2e7b198bc1aec7a7b76ebcb544431a89023a
Nagendra Singh <nagendracoolsingh@gmail.com>
2024-01-15 19:09:21 +0530

    feat: carousel created

---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import styles from './styles.module.css';
+
+const Header = () => {
+  return (
+    <div style={{margin: '2em 0'}}>
+       <div className={styles.header}>
+        SPICE ROUTE CATERS
+    </div>
+
+    <div className={styles.sub_heading}>
+        Catering & Events
+    </div>
+      
+    </div>
+  )
+}
+
+export default Header 
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from './Header/Header'
+import ImageCarousel from './ImageCarousel/ImageCarousel'
+
+const Home = () => {
+  return <div>
+    <Header/>
+    <ImageCarousel/>
+  </div>;
+};
+
+export default Home;
